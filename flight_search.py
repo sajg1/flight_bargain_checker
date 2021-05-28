621f1b7ac03b9668e7baf8d68ff729184715fa4d
Steve Gossip <stephengossip@gmail.com>
2021-05-28 16:01:28 +0100

    I check if the data includes an IATA code. If not, I pass the city to the FlightSearch class and use this to find the relevant code. Currently just passing the string 'TESTING' to test functionality.

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,6 @@
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self, city):
+        self.city = city
+
+    def search_iataCode(self):
+        return "TESTING"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 import os
 import requests
+from flight_search import FlightSearch
+from pprint import pprint
 
 SHEETY_USERNAME = os.environ.get("SHEETY_USERNAME")
 SHEETY_AUTH = os.environ.get("AUTH_HEADER")
@@ -11,4 +13,10 @@ sheety_headers = {
 }
 
 response = requests.get(url=sheety_endpoint, headers=sheety_headers)
-print(response.textd)
+sheet_data = response.json()['prices']
+# pprint(sheet_data)
+for _ in sheet_data:
+    if _['iataCode'] == "":
+        flight_search = FlightSearch(_['city'])
+        _['iataCode'] = flight_search.search_iataCode()
+
